cdc654a6a16a6da3b4e84150c6dc909e1ddbf743
farish-awa <heliuliua@outlook.com>
2024-07-09 13:30:41 +0800

    添加项目文件。

---
+++ MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Foundation;
+using System;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Media.Animation;
+using RivFox.Mail.Views;
+
+namespace RivFox.Mail
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ExtendAcrylicIntoTitleBar();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(1050, 600);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        private void ExtendAcrylicIntoTitleBar()
+        {
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            titleBar.ButtonForegroundColor = Colors.White;
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var selectedItem = (NavigationViewItem)sender.SelectedItem;
+
+            var pageTypes = new Dictionary<string, Type>
+            {
+                { "HomeView", typeof(HomeView) }
+            };
+
+            if (pageTypes.TryGetValue((string)selectedItem.Tag, out var pageType))
+                MainFrame.Navigate(
+                    pageType, null,
+                    new SlideNavigationTransitionInfo
+                    {
+                        Effect = SlideNavigationTransitionEffect.FromLeft
+                    }
+                );
+            else MainFrame.Navigate(
+                    pageType, null,
+                    new SlideNavigationTransitionInfo
+                    {
+                        Effect = SlideNavigationTransitionEffect.FromLeft
+                    }
+                );
+        }
+    }
+}
